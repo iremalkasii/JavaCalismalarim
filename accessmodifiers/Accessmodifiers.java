ce55ea34febc7848a4f644aebbb18c6c6487c30f
iremalkasii <62849473+iremalkasii@users.noreply.github.com>
2020-12-22 00:30:52 +0300

    Add files via upload

---
+++ accessmodifiers/Accessmodifiers.java
@@ -0,0 +1,29 @@
+package accessmodifiers;
+ 
+public class Accessmodifiers {
+
+ 
+    public static void main(String[] args) {
+        
+        Student s= new Student();
+        Student s2;
+        s2 = new Student("irem" ,57, 65);
+        
+        System.out.println(s.getResult());
+        System.out.println(s2.getResult());
+                
+        
+        
+        
+        
+        
+        
+        
+
+    }
+    
+}
+
+    }
+    
+}
