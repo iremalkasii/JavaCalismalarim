405679cceae41b1f676ca8911615af0dec3f4cd8
iremalkasii <62849473+iremalkasii@users.noreply.github.com>
2020-12-22 01:21:16 +0300

    Add files via upload

---
+++ Staticexamples/StaticexmpTest.java
@@ -0,0 +1,23 @@
+
+public class StaticexmpTest {
+    
+    
+    public static void main(String[] args) {
+      Student s1 = new Student(181, "Irem");
+      Student s2 = new Student (182,"Eda");
+      
+      s1.display();
+      s2.display();
+      Student.collage= "IU";
+      s1.display();
+      s2.display();
+      
+
+        
+        
+        
+    }
+ 
+
+    
+}
